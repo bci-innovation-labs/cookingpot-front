d507e9fbbafbb845349e243fa9511212eb70e1bc
unknown <bmika@icloud.com>
2019-04-25 18:25:46 -0400

    BUGFIX: Another submission.

--- src/Components/ContactUsComponent.js
+++ src/Components/ContactUsComponent.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 class ContactUsComponent extends Component{
   render(){
     return(
-      <div>
-      <h1>TODO: HARDY</h1>
-      </div>
+        <div className="container-fluid">
+            <div className="d-flex align-items-stretch">
+                <main id="main" role="main">
+                    <h1>TODO: ROBERT</h1>
+                </main>
+            </div>
+        </div>
     )
   }
 }
